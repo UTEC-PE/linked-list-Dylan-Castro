a04a95c25a41111e0ffa6c18181964578f21fbc9
Dylan Castro Zorrilla <37878046+Dylan-Castro@users.noreply.github.com>
2018-08-28 19:02:54 -0500

    Mi primer commit

--- list.h
+++ list.h
@@ -1,38 +1,179 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <iostream>
+#include "node.h"
+#include "iterator.h"
+
+
+using namespace std;
+
+template <typename T>
 class List {
-    struct Node {
-        int data;
-        struct Node* next;
-
-	void killSelf();
-    };
-
     private:
-        Node* head;
-        Node* tail;
+        Node<T>* head;
+        Node<T>* tail;
         int nodes;
 
-        void print_reverse(Node* head);
+        void print_reverse(Node<T>* head);
 
     public:
         List();
 
-        int front();
-        int back();
-        void push_front(int value);
-        void push_back(int value);
+        T front();
+        T back();
+        void push_front(T value);
+        void push_back(T value);
         void pop_front();
         void pop_back();
-        int get(int position);
+        T get(int position);
+        void concat(List<T> &other);
         bool empty();
         int size();
         void print();
         void print_reverse();
         void clear();
+        Iterator<T> begin();
+        Iterator<T> end();
 
         ~List();
 };
 
+template<class T> List<T>::List()
+{
+
+    head=NULL;
+    tail=NULL;
+
+    nodes=0;
+
+}
+template<class T> List<T>::~List()
+{
+
+}
+
+template<typename T> T List<T>::front()
+{
+
+    return(head->data);
+
+}
+
+template<typename T> T List<T>::back()
+{
+    return(tail->data);
+
+}
+
+template<typename T> int List<T>::size()
+{
+    return nodes;
+}
+
+template<typename T> void List<T>::pop_front()
+{
+    if(head!=tail){
+        head= head->next;
+    }
+}
+template<typename T> void List<T>::pop_back()
+{
+    //incompleto
+    //tail->data= NULL;
+}
+
+template<typename T> bool List<T>::empty()
+{
+    if(head==NULL && tail ==NULL){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+template<typename T> void List<T>::push_front(T value)
+{
+    Node<T>* temp = new Node<T>;
+    temp->data=value;
+    if (head==NULL)
+    {
+        tail=temp;
+
+    }
+    temp->next=head;
+    head=temp;
+    nodes++;
+    
+    temp=NULL;
+    delete temp;
+}
+
+template<typename T> void List<T>::push_back(T value)
+{
+    //  INCOMPLETO
+    /*Node<T>* temp = new Node<T>;
+    temp->data=value;
+    temp->next=NULL;
+    tail->next=temp;
+    tail=temp;
+    if (tail==NULL)
+    {
+        head=temp;
+
+
+    }
+    nodes++;
+    temp=NULL;
+    delete temp;*/
+
+
+}
+
+template<typename T> T List<T>::get(int position)
+{
+    Node<T>* temp=head;
+    T rpta;
+    for(int x=0;x!=position;x++){
+        rpta=temp->data;
+        temp=temp->next;
+    }
+    temp=NULL;
+    delete temp;
+    return rpta;
+}
+
+template<typename T> void List<T>::print()
+{
+    Node<T>* temp=head;
+    while(temp!=tail)
+    {
+        cout<<temp->data<<endl;
+        temp=temp->next;
+
+    }
+    cout << tail->data<<endl    ;
+    temp=NULL;
+    delete temp;
+}
+template<typename T> void List<T>::print_reverse()
+{
+    T lista[nodes];
+    Node<T>* temp=head;
+    int aux=0;
+    while(temp!=tail)
+    {
+        lista[aux]=temp->data;
+        aux++;
+        temp=temp->next;
+
+    }
+    temp=NULL;
+    delete temp;
+    
+    for(int x=nodes;0<x;x--)
+    {
+        cout << lista[x-1] << endl;
+    }
+}
 #endif
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "list.h"
+
+
+int main()
+{
+    List<int>* nombre= new List<int>();
+    //A continuacion los metodos disponibles
+    //cout << nombre->empty();
+    //nombre->push_front(1);
+    //nombre->push_front(2);
+    //nombre->push_front(3);
+    //nombre->push_front(4);
+    //nombre->push_front(5);
+    //nombre->push_front(12);
+    //cout << nombre->front();
+    //cout << nombre->back();
+    //cout << nombre->size();
+    //cout << nombre->empty();
+    //nombre->pop_front();
+    //cout << nombre->get(1);
+    //nombre->print();
+     //nombre->print_reverse();
+    return 0;
+
+}
